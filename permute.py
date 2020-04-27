1700b162d4718b3bccb6d15e83e34e6776ffe2bb
Michael Trossbach <mptrossbach@gmail.com>
2020-04-27 11:11:31 -0400

    Added docstring and assert to check arg is type Sequence

--- permute.py
+++ permute.py
@@ -1,5 +1,18 @@
+from typing import Sequence
+
+
 def permute(seq):
-    '''Permute an iterable. Returns a generator.'''
+    """
+    Permute a sequence.
+
+    Args:
+        seq(Typing.sequence): a sequence to be permuted
+
+    Returns:
+        generator: generator that can be iterated through to return permutations
+    """
+    msg = "Arg 'seq' must be a sequence. See docs for typing.Sequence"
+    assert isinstance(seq, Sequence), msg
     if len(seq) == 2:
         for i in [(seq[0], seq[1]), (seq[1], seq[0])]:
             yield i
